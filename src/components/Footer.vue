e83bedbab0eda177397dfaac2cfcbfaa7ed631d2
s0ci4lx <55864073+s0ci4lx@users.noreply.github.com>
2025-03-10 19:33:30 +0700

    Move Logo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -17,11 +17,6 @@
         ></path>
       </svg>
       <p>Copyright © 2025 - สถานีตำรวจภูธรเทพา</p>
-      <img
-        class="w-12"
-        src="https://www.appsheet.com/template/gettablefileurl?appName=Upload_Picture-2027633&tableName=Picture&fileName=Picture_Images%2F06-20-2023%2019-10-41.Picture.121108.png"
-        alt=""
-      />
     </aside>
   </footer>
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="navbar bg-primary text-primary-content">
-    <button class="btn btn-ghost text-xl">
-      ระบบรายงานการปฏิบัติงาน สภ.เทพา
-    </button>
+    <div class="flex justify-between w-full">
+      <button class="btn btn-ghost text-xl">
+        ระบบรายงานการปฏิบัติงาน สภ.เทพา
+      </button>
+      <img
+        class="w-12"
+        src="https://www.appsheet.com/template/gettablefileurl?appName=Upload_Picture-2027633&tableName=Picture&fileName=Picture_Images%2F06-20-2023%2019-10-41.Picture.121108.png"
+        alt=""
+      />
+    </div>
   </div>
 </template>
